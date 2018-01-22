f76e40136bc0390b76da537c0ca478220b0215a2
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-22 13:20:49 -0800

    implemented ForagerBee in pseudoclassical style
     - inherits from Bee and Bee.prototype
     - added .canFly and .treasureChest properties
     - new .age and .job values
     - new .forage() method

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,21 @@
 var ForagerBee = function() {
-};
+  // inherit differentiated properties from Bee
+  Bee.call(this);
 
+  this.age = 10;
+  this.job = 'find pollen';
+  // .color = 'yellow' inherited from Bee
+  // .food = 'jelly' inherited from Grub via Bee
+
+  this.canFly = true;
+  this.treasureChest = [];
+};
+// inherit shared/similar properties from Bee.prototype
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+// .eat() method inherited from Grub via Bee
+
+ForagerBee.prototype.forage = function forage(treasure) {
+  this.treasureChest.push(treasure);
+}
