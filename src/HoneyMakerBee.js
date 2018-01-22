3af89ce471c80256dd89bdb3fa7a63da29894c2a
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-22 13:15:13 -0800

    implemented HoneyMakerBee in pseudoclassical style
     - .age
     - .job
     - .color inherited from Bee
     - .food inherited from Grub via Bee
     - .honeyPot
     - .eat() inherited from Grub.prototype via Bee.prototype
     - .makeHoney()
     - .giveHoney()

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,24 @@
 var HoneyMakerBee = function() {
+  // inherit differentiated properties from Bee
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'make honey';
+  // inherits .color = 'yellow' from Bee
+  // inherits .food = 'jelly' from Bee <= Grub
+
+  this.honeyPot = 0;
+};
+// inherit shared/similar properties from Bee.prototype
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+// inherits .eat() from Bee.prototype <= Grub.prototype
+
+HoneyMakerBee.prototype.makeHoney = function makeHoney() {
+  this.honeyPot++;
 };
 
+HoneyMakerBee.prototype.giveHoney = function giveHoney() {
+  this.honeyPot--;
+}
