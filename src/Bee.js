9c88d81b951c33314cf1c226cb96fe8af8f99171
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-22 13:05:35 -0800

    implements Bee in pseudoclassical style
     - .age
     - .color
     - .job
     - inherits .food from Grub with Grub.call(this)
     - inherits .eat() from Grub.prototype with Object.create();
     - sets the constructor of Bee.prototype to Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
 var Bee = function() {
+  Grub.call(this);
+
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+
+  // inherits 'food' from Grub
+  // inherits '.eat()' from Grub.prototype
 };
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
 
